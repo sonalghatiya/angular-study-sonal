069e8135b4329e492ddc6bb533b38b423afc3bea
sonalghatiya <sonalghatiya6@gmail.com>
2019-08-11 14:01:34 +0530

    3 files modified, 4 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { SonalComponent } from './sonal/sonal.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, SonalComponent ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
