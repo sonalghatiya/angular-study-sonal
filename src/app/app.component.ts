41145fa0cfeba4dee2e4c91312e20f67bd55d90a
sonalghatiya <sonalghatiya6@gmail.com>
2019-08-11 21:50:14 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
 title = "GameShow";
+
+@Input() game: Game[];
+
+constructor() { 
+
+}
+
+ngOnInit() {
+  this.game[0] = new Game(1, 'Archer', 4);
+  this.game[1] = new Game(2, 'Pokemon', 4);
+  this.game[2] = new Game(3, 'PubG', 4.5);
+}
 }
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -7,16 +7,11 @@ import { Game } from './game'
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
-
-  game: Game[] = [];
-
+  
   constructor() { 
   }
 
   ngOnInit() {
-    this.game[0] = new Game(1, 'Archer', 4);
-    this.game[1] = new Game(2, 'Pokemon', 4);
-    this.game[2] = new Game(3, 'PubG', 4.5);
   }
 
 }
