e3630e1f8c43a15fa4817758b93007af13583857
sonalghatiya <sonalghatiya6@gmail.com>
2019-08-11 13:10:04 +0530

    2 files modified, 3 added

---
+++ src/app/sonal/sonal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sonal',
+  templateUrl: './sonal.component.html',
+  styleUrls: ['./sonal.component.css']
+})
+export class SonalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
